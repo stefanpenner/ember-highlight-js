f33f60d8acc52b2fee989a23313108cea543a07c
Stefan Penner <stefan.penner@gmail.com>
2015-02-01 22:34:14 -0500

    attempt css

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 /* jshint node: true */
 'use strict';
 var path = require('path');
+var find = require('broccoli-stew').find;
+var log = require('broccoli-stew').log;
 
 module.exports = {
   name: 'ember-highlight-js',
+  treeFor: function(name)  {
+      var config = this.app.project.config().emberHighlightJs || {};
+      var style = config.style;
+
+      if (name === 'styles' && style) {
+        return log(find([
+          find(__dirname + '/vendor/highlight.js/styles/{' + style + '}.css'),
+          this._super.treeFor.apply(this, arguments)
+        ]));
+      }
+
+      return this._super.treeFor.apply(this, arguments);
+  },
+
   included: function included(app) {
     this.app = app;
 
